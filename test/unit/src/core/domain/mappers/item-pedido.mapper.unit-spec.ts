b79180b3a2b180d955bb7a54bda141a89983ba7e
Caio <caio.cb.sp@gmail.com>
2024-07-08 12:28:23 -0300

    Unit Test for Mapper Classes

---
+++ test/unit/src/core/domain/mappers/item-pedido.mapper.unit-spec.ts
@@ -0,0 +1,62 @@
+import ItemPedido from "@/core/domain/entities/item-pedido";
+import ItemPedidoDto from "@/core/domain/dto/output/item-pedido.dto";
+import ItemPedidoMapper from "@/core/domain/mappers/item-pedido.mapper";
+import { ProdutoCategoriaEnum } from '@/core/domain/enums/produto-categoria.enum'
+import Produto from '@/core/domain/entities/produto'
+
+describe("Testing ItemPedidoMapper Class", () => {
+    it("toDto static method should receive ItemPedido Class and return ItemPedidoDto class", () => {
+        
+        const produto = new Produto({
+            id: '',
+            nome: '',
+            descricao: '',
+            preco: 1,
+            imagemUrl: '',
+            categoria: ProdutoCategoriaEnum.ACOMPANHAMENTO,
+            ingredientes: [],
+            deletedAt: new Date(1)
+        });
+
+        const itemPedido = new ItemPedido({
+            id: '',
+            produtoId: '',
+            produto: produto,
+            pedidoId: 1,
+            preco: 1,
+            ingredientesRemovidos: []
+        });
+
+        const dto = ItemPedidoMapper.toDto(itemPedido);
+
+        expect(dto.id).toEqual('');
+        expect(dto.produtoId).toEqual('');
+        expect(dto.produto).toEqual(produto);
+        expect(dto.preco).toEqual(1);
+        expect(dto.ingredientesRemovidos).toStrictEqual([]);
+    });
+
+    it("toDomainEntity static method should receive ItemPedidoDto Class and return ItemPedido class", () => {
+        const dto:ItemPedidoDto = {
+            id: '',
+            produtoId: '',
+            produto: {
+                id: '',
+                nome: '',
+                descricao: '',
+                preco: 1,
+                imagemUrl: '',
+                categoria: ProdutoCategoriaEnum.ACOMPANHAMENTO,
+                ingredientes: [],
+                deletedAt: new Date(1)
+            },
+            pedidoId: 1,
+            preco: 1,
+            ingredientesRemovidos: []
+        };
+
+        const itemPedido = ItemPedidoMapper.toDomainEntity(dto);
+
+        expect(itemPedido).toBeInstanceOf(ItemPedido);
+    });
+});
---
+++ test/unit/src/core/domain/mappers/produto.mapper.unit-spec.ts
@@ -0,0 +1,43 @@
+import Produto from '@/core/domain/entities/produto'
+import ProdutoMapper from '@/core/domain/mappers/produto.mapper';
+import { ProdutoCategoriaEnum } from '@/core/domain/enums/produto-categoria.enum';
+
+describe("Testing ProdutoMapper Class", () => {
+    it("toDto static method should receive Produto Class and return ProdutoDto class", () => {
+        const produto = new Produto({
+            id: '',
+            nome: '',
+            descricao: '',
+            preco: 1,
+            categoria: ProdutoCategoriaEnum.ACOMPANHAMENTO,
+            ingredientes: []
+        });
+
+        const dto = ProdutoMapper.toDto(produto);
+
+        expect(dto.id).toEqual('');
+        expect(dto.nome).toEqual('');
+        expect(dto.descricao).toEqual('');
+        expect(dto.preco).toEqual(1);
+        expect(dto.categoria).toEqual(ProdutoCategoriaEnum.ACOMPANHAMENTO);
+        expect(dto.ingredientes).toEqual([]);
+    });
+
+    it("toDomainEntity static method should receive ProdutoDto Class and return Produto class", () => {
+        const dto = {
+            id: '',
+            nome: '',
+            descricao: '',
+            preco: 1,
+            imagemUrl: '',
+            categoria: ProdutoCategoriaEnum.ACOMPANHAMENTO,
+            ingredientes: [],
+            deletedAt: new Date(1)
+        };
+
+        const produto = ProdutoMapper.toDomainEntity(dto);
+
+        expect(produto).toBeInstanceOf(Produto);
+
+    });
+});
