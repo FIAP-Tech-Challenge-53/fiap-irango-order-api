b454a0924e30635b4a476dd2cbbd9bd31151b1bd
Caio <caio.cb.sp@gmail.com>
2024-07-12 17:47:18 -0300

    79 percent of code coverage

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -4,4 +4,8 @@ module.exports = {
   ...config,
   testRegex: '.unit-spec.ts$',
   setupFilesAfterEnv: ['<rootDir>/test/unit/jest.setup.ts'],
+  coveragePathIgnorePatterns: [
+    "src/infra/persistence/typeorm/entities/*",
+    "src/infra/web/nestjs/pedidos/dto/*"
+  ]
 };
