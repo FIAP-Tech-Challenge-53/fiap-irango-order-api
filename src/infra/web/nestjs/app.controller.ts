da71e652ac82453b17e3e455c82fb10da62526d5
Matheus Borges <matob@live.com>
2024-09-11 01:18:18 -0300

    refactor: remove test code

--- src/infra/web/nestjs/app.controller.ts
+++ src/infra/web/nestjs/app.controller.ts
@@ -1,11 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
-import { ApiBody, ApiOkResponse, ApiOperation, ApiProperty } from '@nestjs/swagger'
-
-import { SqsService } from '@ssut/nestjs-sqs'
-import { v4 } from 'uuid'
-
-import { Environment } from '@/infra/web/nestjs/environment'
-import UpdatePedidoRequest from '@/infra/web/nestjs/pedidos/dto/update-pedido.request'
+import { Controller, Get } from '@nestjs/common'
+import { ApiOkResponse, ApiOperation, ApiProperty } from '@nestjs/swagger'
 
 class HealthCheck {
   @ApiProperty({ description: 'Health check status', enum: ['ok', 'error'], example: 'ok' })
@@ -14,7 +8,7 @@ class HealthCheck {
 
 @Controller()
 export default class AppController {
-  constructor (private readonly sqsService: SqsService) { }
+  constructor () { }
 
   @Get()
   @ApiOperation({ summary: 'API index endpoint' })
@@ -28,23 +22,5 @@ export default class AppController {
   @ApiOkResponse({ description: 'Retorna o status da saúde do sistema', type: HealthCheck })
   healthCheck (): HealthCheck {
     return { status: 'ok' }
-  }
-
-  @Post()
-  @ApiOperation({ summary: 'API index sendMessage' })
-  @ApiBody({ type: UpdatePedidoRequest })
-
-  @ApiOkResponse({ description: 'API index sendMessage', type: 'iRango Order sendMessage' })
-  async sendMessage (@Body() body: any) {
-    const message: any = JSON.stringify(body)
-    console.log(body, message)
-    try {
-      await this.sqsService.send(Environment.CONFIRM_PAYMENT_QUEUE, {
-        body: message,
-        id: v4(),
-      })
-    } catch (error) {
-      console.log('error in producing image!', error)
-    }
   }
 }
